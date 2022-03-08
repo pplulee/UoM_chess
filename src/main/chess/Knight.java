8c68f4a2c7fdefff25860932cd68b52ef228975d
022by <46434255+pplulee@users.noreply.github.com>
2022-03-08 20:32:38 +0000

    Completed Knight class

--- src/main/chess/Knight.java
+++ src/main/chess/Knight.java
@@ -1,10 +1,10 @@
 package chess;
 
-class Knight extends Piece{
+public class Knight extends Piece{
 	private PieceColour colour;
 	private String symbol;
 
- 	Knight(PieceColour pc){
+ 	public Knight(PieceColour pc){
 		if (pc.equals(PieceColour.WHITE)){
 			this.colour=PieceColour.WHITE;
 			this.symbol="♘";
@@ -23,7 +23,9 @@ class Knight extends Piece{
 	}
 
 	@Override
-	boolean isLegitMove(int i0, int j0, int i1, int j1) {
-		return true;
+	public boolean isLegitMove(int i0, int j0, int i1, int j1) {
+		 if (Board.hasPiece(i1,j1) && Board.getPiece(i0,j0)!=Board.getPiece(i1,j1)){
+			 return false;
+		 }else return (Math.abs(i0 - i1) == 2 && Math.abs(j0 - j1) == 1) || (Math.abs(i0 - i1) == 1 && Math.abs(j0 - j1) == 2);
 	}
 }
