44e33eb9b41c510a90636eb9d555825f0dad6352
022by <46434255+pplulee@users.noreply.github.com>
2022-03-10 13:44:41 +0000

    Update logic error

--- chess-project/src/main/chess/Knight.java
+++ chess-project/src/main/chess/Knight.java
@@ -24,7 +24,7 @@ public class Knight extends Piece{
 
 	@Override
 	public boolean isLegitMove(int i0, int j0, int i1, int j1) {
-		 if (Board.hasPiece(i1,j1) && Board.getPiece(i0,j0)!=Board.getPiece(i1,j1)){
+		 if (Board.hasPiece(i1,j1) && Board.getPiece(i0,j0).getColour()==Board.getPiece(i1,j1).getColour()){
 			 return false;
 		 }else return (Math.abs(i0 - i1) == 2 && Math.abs(j0 - j1) == 1) || (Math.abs(i0 - i1) == 1 && Math.abs(j0 - j1) == 2);
 	}
